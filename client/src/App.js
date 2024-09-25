4ed2db020914886e6d562d51c2634c0bd2a3cda1
Rayancoder21 <rayan.h.khogali@gmail.com>
2024-09-25 21:15:36 +0300

    buliding homepage with react

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,60 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <main>
+      <header>
+        <a herf="" className="logo">Blogi</a>
+        <nav>
+          <a herf="">Login</a>
+          <a herf="">Register</a>
+        </nav>
       </header>
-    </div>
+
+      <div className="post">
+        <div className="image">
+          <img src="https://techcrunch.com/wp-content/uploads/2024/09/GettyImages-2149377173.jpeg?resize=1280,853" alt="" />
+        </div>
+        <div className="texts">
+          <h2>Ayrton Energy mimics margarine to store hydrogen safely</h2>
+          <p className="info">
+            <a className="author">Tim De Chant</a>
+            <time>2024-09-25 8:54AM</time>
+          </p>
+          <p className="summary">As a fuel, hydrogen has a lot going for it. You can burn it for heat to replace coal, you can run it through an electrolyzer to generate electricity, and you can use it to refuel a vehicle as quickly as gasoline or diesel.</p>
+        </div>
+      </div>
+
+      <div className="post">
+        <div className="image">
+          <img src="https://techcrunch.com/wp-content/uploads/2024/09/GettyImages-2149377173.jpeg?resize=1280,853" alt="" />
+        </div>
+        <div className="texts">
+          <h2>Ayrton Energy mimics margarine to store hydrogen safely</h2>
+          <p className="info">
+            <a className="author">Tim De Chant</a>
+            <time>2024-09-25 8:54AM</time>
+          </p>
+          <p className="summary">As a fuel, hydrogen has a lot going for it. You can burn it for heat to replace coal, you can run it through an electrolyzer to generate electricity, and you can use it to refuel a vehicle as quickly as gasoline or diesel.</p>
+        </div>
+      </div>
+
+      <div className="post">
+        <div className="image">
+          <img src="https://techcrunch.com/wp-content/uploads/2024/09/GettyImages-2149377173.jpeg?resize=1280,853" alt="" />
+        </div>
+        <div className="texts">
+          <h2>Ayrton Energy mimics margarine to store hydrogen safely</h2>
+          <p className="info">
+            <a className="author">Tim De Chant</a>
+            <time>2024-09-25 8:54AM</time>
+          </p>
+          <p className="summary">As a fuel, hydrogen has a lot going for it. You can burn it for heat to replace coal, you can run it through an electrolyzer to generate electricity, and you can use it to refuel a vehicle as quickly as gasoline or diesel.</p>
+        </div>
+      </div>
+      
+        
+    </main>
   );
 }
 
