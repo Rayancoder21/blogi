a4c2d9930f27959a31e5b93ff3f36e3c2579660a
Rayancoder21 <rayan.h.khogali@gmail.com>
2024-09-26 19:20:24 +0300

    Create Post and Header files

---
+++ client/src/Post.js
@@ -0,0 +1,17 @@
+export default function Post() {
+    return (
+        <div className="post">
+            <div className="image">
+                <img src="https://techcrunch.com/wp-content/uploads/2024/09/GettyImages-2149377173.jpeg?resize=1280,853" alt="" />
+            </div>
+            <div className="texts">
+                <h2>Ayrton Energy mimics margarine to store hydrogen safely</h2>
+                <p className="info">
+                    <a className="author">Tim De Chant</a>
+                    <time>2024-09-25 8:54AM</time>
+                </p>
+                <p className="summary">As a fuel, hydrogen has a lot going for it. You can burn it for heat to replace coal, you can run it through an electrolyzer to generate electricity, and you can use it to refuel a vehicle as quickly as gasoline or diesel.</p>
+            </div>
+        </div>
+    );
+}
